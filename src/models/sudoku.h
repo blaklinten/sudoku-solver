fc8f6af0b71bf6f675e3d9741131930c2d023ceb
Lucas Nilsson <lucas@fripost.org>
2023-12-20 12:59:50 +0100

    Test and logic
    - get_row
    - get_column
    - get_box
    - is_valid

--- src/models/sudoku.h
+++ src/models/sudoku.h
@@ -2,15 +2,18 @@
 #define SUDOKU
 
 #include <stdbool.h>
-#include <stdlib.h>
 
-#define SUDOKU_NUMBERS 9
-#define SUDOKU_SIZE SUDOKU_NUMBERS *SUDOKU_NUMBERS
+#define GROUP_SIZE 9
+#define SUDOKU_SIZE GROUP_SIZE *GROUP_SIZE
+#define BOX_SIZE 3
 
-typedef int Sudoku_group[SUDOKU_NUMBERS];
+typedef int Sudoku_group[GROUP_SIZE];
 typedef int Sudoku[SUDOKU_SIZE];
 
-bool assert_valid_sudoku_group(Sudoku_group);
+bool is_solved_group(Sudoku_group);
+bool is_solved(Sudoku);
 Sudoku_group *get_row(int, Sudoku);
+Sudoku_group *get_column(int, Sudoku);
+Sudoku_group *get_box(int, Sudoku);
 
 #endif // !SUDOKU
