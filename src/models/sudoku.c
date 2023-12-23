dfb1f41975f7f4d38023911ddac10086d1498fe6
Lucas Nilsson <lucas@fripost.org>
2023-12-23 09:55:52 +0100

    Using pointers and not values for sudoku type

--- src/models/sudoku.c
+++ src/models/sudoku.c
@@ -1,25 +1,25 @@
 #include <stdlib.h>
 #include "sudoku.h"
 
-bool is_solved(Sudoku s){
+bool is_solved(Sudoku *s){
   for( int i = 0; i < GROUP_SIZE; i++){
     Sudoku_group *row = get_row(i, s);
-    if (!is_solved_group(*row))
+    if (!is_solved_group(row))
       return false;
     Sudoku_group *column = get_column(i, s);
-    if (!is_solved_group(*column))
+    if (!is_solved_group(column))
       return false;
     Sudoku_group *box = get_box(i, s);
-    if (!is_solved_group(*box))
+    if (!is_solved_group(box))
       return false;
   }
   return true;
 }
 
-bool is_solved_group(Sudoku_group numbers) {
+bool is_solved_group(Sudoku_group *numbers) {
   int mask = 0;
   for (int i = 0; i < GROUP_SIZE; i++) {
-    int current = numbers[i];
+    int current = (*numbers)[i];
     mask = mask | 1 << current;
   };
   if (mask != 1022) // 1111111110
@@ -27,31 +27,31 @@ bool is_solved_group(Sudoku_group numbers) {
   return true;
 }
 
-Sudoku_group *get_row(int row_number, Sudoku s) {
+Sudoku_group *get_row(int row_number, Sudoku *s) {
   Sudoku_group *row = calloc(1, sizeof(Sudoku_group));
   int start_index = row_number * GROUP_SIZE;
   for (int i = 0; i < GROUP_SIZE; i++) {
-    (*row)[i] = s[start_index + i];
+    (*row)[i] = (*s)[start_index + i];
   }
   return row;
 }
 
-Sudoku_group *get_column(int column_number, Sudoku s) {
+Sudoku_group *get_column(int column_number, Sudoku *s) {
   Sudoku_group *column = calloc(1, sizeof(Sudoku_group));
   for (int i = 0; i < GROUP_SIZE; i++) {
-    (*column)[i] = s[column_number + GROUP_SIZE * i];
+    (*column)[i] = (*s)[column_number + GROUP_SIZE * i];
   }
   return column;
 }
 
-Sudoku_group *get_box(int box_number, Sudoku s) {
+Sudoku_group *get_box(int box_number, Sudoku *s) {
   Sudoku_group *box = calloc(1, sizeof(Sudoku_group));
   int start_row = (box_number / BOX_SIZE) * BOX_SIZE;
   int start_column = (box_number % BOX_SIZE) * BOX_SIZE;
   int start_index = start_row * GROUP_SIZE + start_column;
   for (int i = 0; i < BOX_SIZE; i++) {
     for (int j = 0; j < BOX_SIZE; j++){
-      (*box)[i*BOX_SIZE + j] = s[start_index + (i * GROUP_SIZE) + j];
+      (*box)[i*BOX_SIZE + j] = (*s)[start_index + (i * GROUP_SIZE) + j];
     }
   }
   return box;
--- test/sudoku_test.c
+++ test/sudoku_test.c
@@ -33,11 +33,11 @@ Sudoku unfinished = {1, 2, 3, 4, 5, 6, 7, 8, 9,
 
 bool test_get_row() {
   s_log(INFO, __func__, "Running");
-  Sudoku_group *row0 = get_row(0, solved);
+  Sudoku_group *row0 = get_row(0, &solved);
   Sudoku_group expected0 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  Sudoku_group *row3 = get_row(3, solved);
+  Sudoku_group *row3 = get_row(3, &solved);
   Sudoku_group expected3 = {2, 3, 4, 5, 6, 7, 8, 9, 1};
-  Sudoku_group *row8 = get_row(8, solved);
+  Sudoku_group *row8 = get_row(8, &solved);
   Sudoku_group expected8 = {9, 1, 2, 3, 4, 5, 6, 7, 8};
   if (!groups_equals(row0, &expected0) ||
       !groups_equals(row3, &expected3) ||
@@ -57,11 +57,11 @@ bool test_get_row() {
 
 bool test_get_column() {
   s_log(INFO, __func__, "Running");
-  Sudoku_group *column0 = get_column(0, solved);
+  Sudoku_group *column0 = get_column(0, &solved);
   Sudoku_group expected0 = {1, 4, 7, 2, 5, 8, 3, 6, 9};
-  Sudoku_group *column4 = get_column(4, solved);
+  Sudoku_group *column4 = get_column(4, &solved);
   Sudoku_group expected4 = {5, 8, 2, 6, 9, 3, 7, 1, 4};
-  Sudoku_group *column8 = get_column(8, solved);
+  Sudoku_group *column8 = get_column(8, &solved);
   Sudoku_group expected8 = {9, 3, 6, 1, 4, 7, 2, 5, 8};
   if (!groups_equals(column0, &expected0) ||
       !groups_equals(column4, &expected4) ||
@@ -81,11 +81,11 @@ bool test_get_column() {
 
 bool test_get_box() {
   s_log(INFO, __func__, "Running");
-  Sudoku_group *box0 = get_box(0, solved);
+  Sudoku_group *box0 = get_box(0, &solved);
   Sudoku_group expected0 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  Sudoku_group *box4 = get_box(4, solved);
+  Sudoku_group *box4 = get_box(4, &solved);
   Sudoku_group expected4 = {5, 6, 7, 8, 9, 1, 2, 3, 4};
-  Sudoku_group *box8 = get_box(8, solved);
+  Sudoku_group *box8 = get_box(8, &solved);
   Sudoku_group expected8 = {9, 1, 2, 3, 4, 5, 6, 7, 8};
   if (!groups_equals(box0, &expected0) ||
       !groups_equals(box4, &expected4) ||
@@ -107,19 +107,19 @@ bool test_is_solved_group() {
   s_log(INFO, __func__, "Running");
 
   Sudoku_group valid = {9, 2, 7, 4, 3, 6, 1, 5, 8};
-  if (!is_solved_group(valid)) {
+  if (!is_solved_group(&valid)) {
     s_log(ERROR, __func__, "Group shoud be valid");
     return false;
   }
 
   Sudoku_group invalid = {1, 1, 3, 4, 5, 6, 7, 8, 9};
-  if (is_solved_group(invalid)) {
+  if (is_solved_group(&invalid)) {
     s_log(ERROR, __func__, "Group shoud not be valid");
     return false;
   }
 
   Sudoku_group too_few = {1, 2, 3};
-  if (is_solved_group(too_few)) {
+  if (is_solved_group(&too_few)) {
     s_log(ERROR, __func__, "Group shoud not be valid");
     return false;
   }
@@ -129,15 +129,15 @@ bool test_is_solved_group() {
 
 bool test_is_solved() {
   s_log(INFO, __func__, "Running");
-  if(!is_solved(solved)){
+  if(!is_solved(&solved)){
     s_log(ERROR, __func__, "This Sudoku IS correctly solved");
     return false;
   }
-  if(is_solved(unsolved)){
+  if(is_solved(&unsolved)){
     s_log(ERROR, __func__, "This Sudoku is NOT correctly solved");
     return false;
   }
-  if(is_solved(unfinished)){
+  if(is_solved(&unfinished)){
     s_log(ERROR, __func__, "This Sudoku is NOT done");
     return false;
   }
